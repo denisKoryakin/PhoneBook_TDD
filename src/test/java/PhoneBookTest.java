daa0d85f57cb5350fcfe2f5173125e39d0dfd9d1
Denis Koryakin <singler1991@gmail.com>
2023-03-06 12:40:40 +0500

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,42 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
 public class PhoneBookTest {
+    @BeforeAll
+    public static void beforeAllTests() {
+        System.out.println("All PhoneBookTests started");
+    }
+
+    @AfterAll
+    public static void afterAllTests() {
+        System.out.println("All PhoneBookTests completed");
+    }
+
+    PhoneBook phoneBook = new PhoneBook();
+
+    @ParameterizedTest
+    @MethodSource("addParametersDefinition")
+    public void testAdd(String name, String number, int count) {
+//    arrange
+//    act
+        int result = phoneBook.add(name, number);
+//    assert
+        Assertions.assertEquals(count, result);
+    }
+
+    private static Stream<Arguments> addParametersDefinition() {
+        return Stream.of(
+                Arguments.of("Иван", "89527812756", 1),
+                Arguments.of("Ирина", "87538956759", 2),
+                Arguments.of("Иван", "89529532658", 2),
+                Arguments.of("Петр", "89634478956", 3)
+        );
+    }
 }
