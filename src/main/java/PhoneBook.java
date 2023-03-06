a1221d3f073b873b171fcecdddeac8c90338a5ec
Denis Koryakin <singler1991@gmail.com>
2023-03-06 12:58:02 +0500

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,6 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
 
+    private Map<String, String> contacts = new HashMap<>();
+    private static PhoneBook instance = null;
+
+    private PhoneBook() {}
+
+    public static PhoneBook get() {
+        if(instance ==null) {
+            instance = new PhoneBook();
+        }
+        return instance;
+    }
+
     public int add(String name, String number) {
-        return 0;
+        if (!contacts.containsKey(name)) {
+            contacts.put(name, number);
+        }
+        return contacts.size();
     }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -19,7 +18,7 @@ public class PhoneBookTest {
         System.out.println("All PhoneBookTests completed");
     }
 
-    PhoneBook phoneBook = new PhoneBook();
+    PhoneBook phoneBook = PhoneBook.get();
 
     @ParameterizedTest
     @MethodSource("addParametersDefinition")
